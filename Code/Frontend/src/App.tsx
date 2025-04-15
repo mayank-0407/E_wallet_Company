e280e1ca886feb7f2c6f67e1080da1981dc8011a
Mayank Aggarwal <mayank.aggarwal@mphasis.com>
2025-04-15 11:11:42 +0530

    Initialised the frontend

---
+++ Code/Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-600">Hi mayank</h1>
+    </>
+  );
+}
+
+export default App;
